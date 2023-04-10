0d5d251e674227cd1333f15084b61a0785c60f6f
Ryan Rudd <85949246+TechPenguineer@users.noreply.github.com>
2023-04-09 21:53:47 -0400

    utilize multiple CPU cores

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 import sys
 import yaml
 import logging
-from moviepy.editor import *
 import platform
+from moviepy.editor import *
+from multiprocessing import Process, Manager
 
 
 logging.basicConfig(level=logging.INFO)
@@ -17,27 +18,51 @@ def ReadConfig(config_file: str = "./util/config.yaml"):
         logger.info('Log read...')
     logger.info('Returning YAML safe load...')
     return yaml.safe_load(configDoc)
-CONFIG = ReadConfig()
 
-VIDEO_SIZE = (CONFIG['VIDEO_RESOLUTION']['VIDEO_RES_X'],CONFIG['VIDEO_RESOLUTION']['VIDEO_RES_Y'])
-DURATION = CONFIG['VIDEO_TEST_SETTINGS']['SAMPLE_RES_TEST_DURATION']
-FPS = CONFIG['VIDEO_SETTINGS']['FPS']
-OUTPUT_FOLDER = CONFIG['VIDEO_SETTINGS']['OUTPUT_FOLDER']
-OUTPUT_FILE = CONFIG['VIDEO_SETTINGS']['OUTPUT_FILE']
-OUTPUT_TYPE = CONFIG['VIDEO_SETTINGS']['OUTPUT_TYPE']
 
-def color_clip(size, duration, fps=25, color=(0,0,0), output='color.mp4'):
-    logger.info('Creating color clip...')
+def color_clip_process(size, duration, fps, color, output, process_num, results):
+    logger.info(f'Process {process_num}: Creating color clip...')
     clip = ColorClip(size, color, duration=duration)
-    logger.info('Writing video file...')
+    logger.info(f'Process {process_num}: Writing video file...')
     clip.write_videofile(output, fps=fps)
-    logger.info(f'Video file saved to {output}')
+    logger.info(f'Process {process_num}: Video file saved to {output}')
+    results[process_num] = output
 
-if not os.path.exists(OUTPUT_FOLDER):
-    os.mkdir(OUTPUT_FOLDER)
 
-os.system('cls' if os.name == 'nt' else 'clear')
-color_clip(VIDEO_SIZE, DURATION, FPS, (0,0,0), f"{OUTPUT_FOLDER}/{OUTPUT_FILE}.{OUTPUT_TYPE}")
-os.system('cls' if os.name == 'nt' else 'clear')
+if __name__ == '__main__':
+    CONFIG = ReadConfig()
 
-print(f"Py2TokVid - {platform.system()} Version")
+    VIDEO_SIZE = (CONFIG['VIDEO_RESOLUTION']['VIDEO_RES_X'],CONFIG['VIDEO_RESOLUTION']['VIDEO_RES_Y'])
+    DURATION = CONFIG['VIDEO_TEST_SETTINGS']['SAMPLE_RES_TEST_DURATION']
+    FPS = CONFIG['VIDEO_SETTINGS']['FPS']
+    OUTPUT_FOLDER = CONFIG['VIDEO_SETTINGS']['OUTPUT_FOLDER']
+    OUTPUT_FILE = CONFIG['VIDEO_SETTINGS']['OUTPUT_FILE']
+    OUTPUT_TYPE = CONFIG['VIDEO_SETTINGS']['OUTPUT_TYPE']
+    NUM_PROCESSES = CONFIG.get('NUM_PROCESSES', os.cpu_count())
+
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.mkdir(OUTPUT_FOLDER)
+
+    manager = Manager()
+    results = manager.dict()
+    processes = []
+    for i in range(NUM_PROCESSES):
+        output = f"{OUTPUT_FOLDER}/{OUTPUT_FILE}_{i}.{OUTPUT_TYPE}"
+        process = Process(target=color_clip_process, args=(VIDEO_SIZE, DURATION, FPS, (0,0,0), output, i, results))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    final_output = f"{OUTPUT_FOLDER}/{OUTPUT_FILE}.{OUTPUT_TYPE}"
+    with VideoFileClip(results[0]) as final_clip:
+        for i in range(1, NUM_PROCESSES):
+            clip = VideoFileClip(results[i])
+            final_clip = concatenate_videoclips([final_clip, clip])
+        logger.info('Concatenating video clips...')
+        final_clip.write_videofile(final_output, fps=FPS)
+        logger.info(f'Final video file saved to {final_output}')
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"Py2TokVid - {platform.system()} Version")
