6692539e202c5cb13e4184489e3d888175c6aacf
Ryan Rudd <85949246+TechPenguineer@users.noreply.github.com>
2023-04-09 21:58:12 -0400

    coloring

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from multiprocessing import Pool
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Define custom log formatter with color and weight
+log_formatter = logging.Formatter("\033[1m%(asctime)s [%(levelname)s] %(name)s:\033[0m %(message)s")
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+logger.addHandler(console_handler)
+
 
 def read_config(config_file: str = "./util/config.yaml") -> dict:
     logger.info('Opening config...')
@@ -24,7 +30,7 @@ def create_color_clip(args):
     clip = ColorClip(size, color, duration=duration)
     logger.info(f'Process {process_num}: Writing video file...')
     clip.write_videofile(output, fps=fps)
-    logger.info(f'Process {process_num}: Video file saved to {output}')
+    logger.info(f'Process {process_num}: \033[32mVideo file saved to {output}\033[0m')
     return output
 
 
@@ -48,9 +54,9 @@ if __name__ == '__main__':
     final_output = f"{output_folder}/{output_file}.{output_type}"
     clips = [VideoFileClip(r) for r in results]
     final_clip = concatenate_videoclips(clips)
-    logger.info('Concatenating video clips...')
+    logger.info('\033[34mConcatenating video clips...\033[0m')
     final_clip.write_videofile(final_output, fps=fps)
-    logger.info(f'Final video file saved to {final_output}')
+    logger.info(f'\033[32mFinal video file saved to {final_output}\033[0m')
 
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(f"Py2TokVid - {platform.system()} Version")
+    print(f"\033[1mPy2TokVid\033[0m - {platform.system()} Version")
